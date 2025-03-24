054c27ade7d5e981054e454ab1cad92f373e7721
Bubulle <bubulle86@yahoo.fr>
2025-03-24 18:06:41 +0100

    Continuing Map layout

--- lib/src/models/matching_timetable.dart
+++ lib/src/models/matching_timetable.dart
@@ -13,7 +13,6 @@ class MatchingTimetable extends Timetable {
   @override
   Iterable<StopTime> getNext({DateTime? from}) {
     from ??= DateTime.now();
-    // return gtfsTimeTable.getNext(from: from);
     List<StopTime> apiTimes = realTime.getNext(from: from).toList();
     final result = apiTimes.where((e) => e.isRealTime).map((e) => gtfsTimeTable.matchTime(e)).toList();
     final Set<int> matchedTrips = result.map((e) => e.trip?.id ?? -1).toSet();
